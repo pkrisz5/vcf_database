5faeb6a2670070999a351f1151d7a1953e28e3fa
pkrisz5 <pkrisz5@gmail.com>
2021-06-25 11:56:02 +0200

    Create lineage_def_script.R

---
+++ scripts/lineage_def_script.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(DBI)
+library(RPostgreSQL)
+
+print(paste(Sys.time(), "started...", sep = " "))
+
+
+load("../data/lineage_def.Rdata") #TODO: use env var to point to extra data folder
+
+
+con <- DBI::dbConnect(RPostgreSQL::PostgreSQL(),
+                      dbname = Sys.getenv(c("DB")),
+                      host = Sys.getenv(c("DB_HOST")),
+                      port = Sys.getenv(c("DB_PORT")),
+                      user = Sys.getenv(c("SECRET_USERNAME")),
+                      password = Sys.getenv(c("SECRET_PASSWORD"))
+)
+dbSendQuery(con, "TRUNCATE TABLE lineage_def")
+print(paste(Sys.time(), "truncated table lineage_def", sep=" ")) 
+
+dbWriteTable(con, "lineage_def", lineage_def , row.names = FALSE, overwrite = TRUE)
+
+dbDisconnect(con)
+
+
