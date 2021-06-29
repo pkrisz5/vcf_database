df435d664d241b931ea71b900f8c77f329c8bf3a
Steger, Jozsef <steger@complex.elte.hu>
2021-06-29 10:04:16 +0200

    operation table

---
+++ schema/table-operation.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS operation (
+	event_date date,
+	last_stage int,
+	last_exit_code int,
+	stage int,
+        exit_code int,
+	extra_info text -- json encoded information
+);
