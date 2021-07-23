403eae7e3ca3cac8933539adec1264ceb4de9623
pkrisz5 <pkrisz5@gmail.com>
2021-07-23 11:41:49 +0200

    Update table-ecdc_covid_country_weekly.sql

--- schema/table-ecdc_covid_country_weekly.sql
+++ schema/table-ecdc_covid_country_weekly.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS ecdc_covid_country_weekly(
     iso_a2                            text,
     country_name                      text,
     country_name_local                text,
+    population                        real,
     date_year                         real,
     date_week                         real,
     ecdc_covid_country_weekly_cases   real,
